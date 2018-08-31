84d3a2f6e332c0c7de4586a9e3b6dbf68f6a398a
Verdan Mahmood <verdan.mahmood@gmail.com>
2018-08-31 11:17:24 +0200

    Make use of factory function

--- app.py
+++ app.py
@@ -2,24 +2,30 @@ from flask import Flask
 from flask_oidc import OpenIDConnect
 
 from config import config
-app = Flask(__name__)
 
-# Load the configurations based on the 'FLASK_ENV' environment variable
-app.config.from_object(config)
+app = None
+oidc = OpenIDConnect()
 
-# Init the OpenIDConnect application instance
-oidc = OpenIDConnect(app)
 
-# Registers the API URLs
-# Below import here is to prevent the circular imports
-from backend.apis import api as api_blueprint
-api_prefix = '/{}/api'.format(app.config.get('SERVICE_SLUG'))
-app.register_blueprint(api_blueprint, url_prefix=api_prefix)
+def create_app():
+    global app, oidc
+    app = Flask(__name__)
 
-# Registers the Views URLs
-# This import here is to prevent the circular imports
-from backend.views import view as view_blueprint
-app.register_blueprint(view_blueprint)
+    # Load the configurations based on the 'FLASK_ENV' environment variable
+    app.config.from_object(config)
 
-if __name__ == '__main__':
-    app.run()
+    # Init the OpenIDConnect application instance
+    oidc.init_app(app)
+
+    # Registers the API URLs
+    # Below import here is to prevent the circular imports
+    from backend.apis import api as api_blueprint
+    api_prefix = '/{}/api'.format(app.config.get('SERVICE_SLUG'))
+    app.register_blueprint(api_blueprint, url_prefix=api_prefix)
+
+    # Registers the Views URLs
+    # This import here is to prevent the circular imports
+    from backend.views import view as view_blueprint
+    app.register_blueprint(view_blueprint)
+
+    return app
